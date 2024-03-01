32ca31a42bd66553664fd4111697a9ead92e18e2
Shakya Tilak <shakyatilak45@gmail.com>
2024-03-01 22:55:07 +0530

    Initial setup

--- FireNews/Controllers/NewsListViewController.swift
+++ FireNews/Controllers/NewsListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NewsListViewController.swift
 //  FireNews
 //
 //  Created by Tilak Shakya on 01/03/24.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewsListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
-
